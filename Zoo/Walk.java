9bb1678c81ee359fa2c490e8bb2f3899ae01f75e
Bill Vo (Work) <gvo3@gatech.edu>
2026-03-24 09:01:15 -0400

    Completed Walk interface

--- Zoo/Walk.java
+++ Zoo/Walk.java
@@ -1,9 +1,4 @@
 public interface Walk {
-
-    /** TODO 4: declare a functionality or method named
-
-     *          "walking" with return type "void"
-
-     */
-
+    // Declaring a method named "walking" with return type "void"
+    public void walking();
 }
